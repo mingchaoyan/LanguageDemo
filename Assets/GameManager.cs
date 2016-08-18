778afb05de1420f7e0fb9541aa94c82bb202c493
mingchaoyan <mingchaoyan@gmail.com>
2016-08-18 17:37:15 +0800

    init

---
+++ Assets/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	public void OnClickButton() {
+		string sceneName = SceneManager.GetActiveScene().name;
+		Debug.Log(sceneName);
+
+		if(sceneName == "Main") {
+			SceneManager.LoadScene(1);
+		} else {
+			SceneManager.LoadScene(0);
+		}
+	}
+
+}
